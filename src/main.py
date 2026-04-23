853817909ce112adfd520a2fbfef12725c9a9411
gaia <gayathiri.ravendirane@gmail.com>
2026-04-23 17:39:55 +0200

    update du main

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ from subgradients import subgradient_basic, subgradient_Polyak, subgradient_ADS
 from readData_progDyn import *
 from progDyn import relax1, relax2, optimalOrRealisableOrInfesable
 from test_progDyn import *
+from pathlib import Path
 
 import numpy as np
 
 def demo_progDyn():
-    datafilePath = 'data/wt040/wt040_001.dat'
+    project_root = Path(__file__).resolve().parents[1]
+    datafilePath = project_root / 'data_aone' / 'wt040' / 'wt040_001.dat'
 
     nbItems, T, processingTimes, dueDates, penalties = readData(datafilePath)
     L_tab, sequence = relax1(nbItems, T, processingTimes, dueDates, penalties)
@@ -22,6 +24,8 @@ def main():
 
     # Example on P2 
     print(subgradient_Polyak(np.array([3.0]), np.array([]), 0.000001, problem_name="P2")[0])
+
+    # demo_progDyn()
 
 if __name__ == "__main__":
     main()
