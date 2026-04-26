76722eef47be5a010dbe6ac77bb133cb146d2401
gaia <gayathiri.ravendirane@gmail.com>
2026-04-26 14:37:44 +0200

    added rapport

--- src/algoP3.py
+++ src/algoP3.py
@@ -1,5 +1,9 @@
-def p3_compute_dual_function(instance: SchedulingInstance, Ti, pi, mu):
-    # dual_val=0
+import numpy as np
+from problems import SchedulingInstance, p3_cost, lagrange_cost
+
+
+def p3_compute_dual_function_ResolutionPB(instance: SchedulingInstance, Ti, pi, mu):
+    dual_val=0
     x_opt= np.zeros((instance.nb_jobs, instance.horizon+1))
 
     for i in range(instance.nb_jobs):
@@ -12,12 +16,12 @@ def p3_compute_dual_function(instance: SchedulingInstance, Ti, pi, mu):
             sum_mu= np.sum(mu[t:t+p_i])
             lagrangien= (cost +sum_mu)* x_opt[i,t]
 
-            # if lagrange_cost < min_cost:
-            #     min_cost=lagrange_cost
-            #     best_t=t
+            if lagrange_cost < min_cost:
+                min_cost=lagrange_cost
+                best_t=t
         
-        # x_opt[i,best_t]=1
-        # dual_val += min_cost
+        x_opt[i,best_t]=1
+        dual_val += min_cost
     
     lagrangien -= np.sum(mu) 
     return lagrangien, x_opt
